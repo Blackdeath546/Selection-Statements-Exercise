3ab48f33203708b265bec09dfe2b67488e0cb196
Nicholas Angeles <blackdeath546@gmail.com>
2024-10-16 19:43:59 -0400

    Added Guessing Game for favorite random number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,70 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+
+        static int RandomNumber()
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            //var userInput = int.Parse(Console.ReadLine());
+            return favNumber;
+        }
+
+        
+
+         static int GuessingGame()
+         {
+             int favNumber = RandomNumber();
+             
+
+             int GuessingLoop()
+             {
+                 
+                 var userInput = double.Parse(Console.ReadLine());
+                 
+                 if (userInput == favNumber)
+                 {
+                     Console.WriteLine($"You Win! My favorite number is {favNumber}");
+                 }
+                 else if (userInput > favNumber)
+                 {
+                     Console.WriteLine($"Too high try again!");
+
+                     GuessingLoop();
+                 }
+                 else if (userInput < favNumber)
+                 {
+                     Console.WriteLine($"Too low try again!");
+
+                     GuessingLoop();
+                 }
+                 else 
+                 {
+                     Console.WriteLine("Please enter a valid number"); //Unable to fix bug where putting anything but a number breaks the build.
+
+                     GuessingLoop();
+                 }
+
+                 return favNumber; // it says I need a return, but it doesn't do anything from my understanding
+             }
+             GuessingLoop();
+             return favNumber;
+         }
+
+        static void Main(string[] args)
+        { 
+            //RandomNumber();
+            //Console.WriteLine($"{RandomNumber()}");
+            //Console.WriteLine($"{favNumber}");
+            //Console.WriteLine($"{favNumber}");
+            
+            //Console.WriteLine("Hmmm I got it! ");
+            Console.WriteLine("Lets try to guess my favorite number!");
+            Console.WriteLine("Guess my favorite number! : ");
+           
+            GuessingGame();
+            
+            
         }
     }
 }
